e2ac8b6dc743dacd7156a0b588e753dc67def68c
peskov.igor <peskow.igor@gmail.com>
2023-04-16 00:11:46 +0400

    commit

---
+++ main/tasks.php
@@ -0,0 +1,34 @@
+ <?php
+
+    print "<table border='1'>";
+        for ($i
+=1; $i
+<=100; $i
+++){
+            print "<tr>";
+                print "<td> $i
+ </td>";
+            print "</tr>";
+        }
+    print "</table>"; 
+    
+
+print "<table border='1'>";
+        for ($i=1; $i<=100; $i++){
+            print "<tr>";
+                print "<td> $i
+ </td>";
+            print "</tr>";
+        }
+    print "</table>"; 
+    
+
+
+
+
+
+
+
+
+
+?>
