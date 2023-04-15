cd1f500eb9a4194e744a1ad717e8b9fb3b26aa7c
Кирилл <vtorcevkirill@mail.ru>
2023-04-16 00:56:22 +0400

    123

--- main/index.php
+++ main/index.php
@@ -28,6 +28,7 @@
 
                     <a class="nav-item" href="@">Цены</a>
                     <a class="nav-item" href="@">О больнице</a>
+                    <a class="nav-item" href="tasks.html">Задания</a>
                     
                 </div>
                 
@@ -99,31 +100,8 @@
                     </TR>   
                   </TABLE>
                 </p>
-
-                Ваше имя: <br>
-                <input type="text" name="login" placeholder="Имя" />
- 
-<form name="form" action="" method="get">
-  <input type="text" name="subject" id="subject" value="Car Loan">
-  <button type="submit">123</button>
-</form>
-
-<?php 
-
-echo $_GET['subject'];
-
-print "<br>";
-
-for ($i=1; $i<=100; $i++){
-    print $i;
-    print " ";
-
-    if (fmod($i, 10) == 0) {
-        print "<br>";
-    }
-}
-
-?>
+            </div>
+                
 
         </div>
     </div>
