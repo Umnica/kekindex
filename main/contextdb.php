dd81d8ceede6aba9c10719cc3fe358fd718a91a0
Кирилл <vtorcevkirill@mail.ru>
2023-04-21 23:32:20 +0400

    MySQL

--- main/contextdb.php
+++ main/contextdb.php
@@ -1,9 +1,18 @@
 <?php
+$servername = "(localdb)\MSSqlLocalDB";
+//$servername = "localhost";
+$username = "DESKTOP-UFIC4SL\Kirill";
+//$username = "root";
+$password = "";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-mysql_connect("localhost", "root", ""); //подключение к БД
-mysql_select_db("query");             // выбор БД
-mysql_set_charset('utf8');  
-
-
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
+
+
